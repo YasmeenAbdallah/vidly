aeeadf156bb45f9879f9595152ab01faa431e6c8
yasmeen abdallah <yasmeenabdallah11898@gmail.com>
2021-06-25 16:00:15 +0200

    Add project files.

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var movie = new movie() { Name="shrek!"};
+            return Content("hello ");
+        }
+        public IActionResult Edit(int id)
+        {
+            return Content("id " + id);
+        }
+        public IActionResult SortBy(int? id, string name)
+        {
+            if (!id.HasValue)
+            {
+                id = 1;
+            }
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "this is the first one";
+            }
+            return Content(string.Format("hello the id is{0} and sort by{1}", id, name));
+        }
+    }
+}
---
+++ vidly/Models/movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vidly.Models
+{
+    public class movie
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
